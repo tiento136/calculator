d7960c82a0cae938f02f2a47dc49c66e39fa908e
unknown <2154050298tien@ou.edu.vn>
2023-10-27 11:39:01 +0700

    cộng

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,14 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a + b;
+            textBox3.Text = c.ToString();
+
+        }
     }
 }
